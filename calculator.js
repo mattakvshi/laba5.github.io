2ab1895e615ecdcd97029913e43cdc16b95f4631
Максим Сидоренко <108477421+mattakvshi@users.noreply.github.com>
2022-10-11 18:40:14 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,24 +1,24 @@
-
-function proiz() {
-    var num1, num2, result;
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
-    var regex = /^[1-9]\d+$/;
-    if (!regex.test(num1) || !regex.test(num2)) {
-        alert("Пожалуйста, введите корректные числа.");
-        return false;
-    }
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    result = num1 * num2;
-    alert("Вы собираетесь умножить 2 числа: "+num1+" и "+num2+".");
-    document.getElementById('out').innerHTML = "Результат: "+result;
-}
+function proiz(){
+    const check = /^-+\d+$|\d+$/;
+    let num1 = document.getElementsByName("n1");
+    let num2 = document.getElementsByName("n2");
     
+    if (check.test(num1[0].value) == false|| check.test(num2[0].value) == false) {
+        document.getElementById('out').innerHTML='Вы сделали что-то не так!';
+    }
+    else{
+        let num3 = document.getElementById("out");
+        num1[0] = Number.parseInt(num1[0]);
+        num2[0] = Number.parseInt(num2[0]);
+        var result = num1[0].value * num2[0].value;
+        num3.innerHTML = Math.abs(result);   
+    }
+}
 
-window.addEventListener('DOMContentLoaded', function (event) {
-    console.log("DOM fully loaded and parsed");
+    
+window.addEventListener("DOMContentLoaded", function(event)  {
+    console.log("DOM loaded ^_^");
 
-    let b = document.getElementById("proiz");
-    b.addEventListener("Пожалуйста, введите корректные числа.", proiz);
+    let buttonProiz = document.getElementById("proiz");
+    buttonProiz.addEventListener("Click",proiz);
 });
